0692f5cd01297cbabd19c28191c40e83c53d2db7
Saranga Malshan <sarangavp36@gmail.com>
2024-09-04 23:03:17 +0530

    Get text chunks

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
 def extract_pdf_text(pdfs):
     text = ""
@@ -9,6 +10,16 @@ def extract_pdf_text(pdfs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
 
 def main():
@@ -21,9 +32,9 @@ def main():
         st.subheader("Your files")
         pdfs =st.file_uploader("Upload your PDFs here", accept_multiple_files=True)
         if st.button("Enter"):
-            raw_text = extract_pdf_text(pdfs)
-            #st.write(raw_text)
-
+            text = extract_pdf_text(pdfs)
+            chunk_text = get_text_chunks(text)
+            st.write(chunk_text)
 
 if __name__ == '__main__':
     main()
